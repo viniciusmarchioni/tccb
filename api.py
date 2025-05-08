c1b6cfeb28c227dd507b2789fb30a487682e979e
EnzoSanchez23 <enzosanchez2302@gmail.com>
2025-05-08 19:50:13 -0300

    altera modelo de previsão

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from fetchDBHelper import *
 from flask_cors import CORS
-from previsao_XGBoost import prever_resultado
+from previsao_IA import prever_resultado
 
 app = Flask(__name__)
 CORS(app)
